0b5907dbc65b67cd6b96ca9e11d94b10e9ea41ac
Santiago Penagos Uribe <santipuribe@gmail.com>
2019-09-15 11:58:08 -0500

    Retoma de aprendizaje al momento de realizar evaluacion

---
+++ Notas/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Notas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.txtn4 = new System.Windows.Forms.TextBox();
+            this.txtn5 = new System.Windows.Forms.TextBox();
+            this.btnvalidar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblpromedio = new System.Windows.Forms.Label();
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Promedio de Notas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese sus notas:";
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(170, 101);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 20);
+            this.txtn1.TabIndex = 2;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(170, 136);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 20);
+            this.txtn2.TabIndex = 3;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(170, 171);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(100, 20);
+            this.txtn3.TabIndex = 4;
+            // 
+            // txtn4
+            // 
+            this.txtn4.Location = new System.Drawing.Point(170, 217);
+            this.txtn4.Name = "txtn4";
+            this.txtn4.Size = new System.Drawing.Size(100, 20);
+            this.txtn4.TabIndex = 5;
+            // 
+            // txtn5
+            // 
+            this.txtn5.Location = new System.Drawing.Point(170, 255);
+            this.txtn5.Name = "txtn5";
+            this.txtn5.Size = new System.Drawing.Size(100, 20);
+            this.txtn5.TabIndex = 6;
+            // 
+            // btnvalidar
+            // 
+            this.btnvalidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvalidar.Location = new System.Drawing.Point(113, 317);
+            this.btnvalidar.Name = "btnvalidar";
+            this.btnvalidar.Size = new System.Drawing.Size(97, 43);
+            this.btnvalidar.TabIndex = 7;
+            this.btnvalidar.Text = "Validar";
+            this.btnvalidar.UseVisualStyleBackColor = true;
+            this.btnvalidar.Click += new System.EventHandler(this.btnvalidar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado:";
+            // 
+            // lblpromedio
+            // 
+            this.lblpromedio.AutoSize = true;
+            this.lblpromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpromedio.Location = new System.Drawing.Point(133, 397);
+            this.lblpromedio.Name = "lblpromedio";
+            this.lblpromedio.Size = new System.Drawing.Size(76, 20);
+            this.lblpromedio.TabIndex = 9;
+            this.lblpromedio.Text = "Promedio";
+            this.lblpromedio.Visible = false;
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltexto.Location = new System.Drawing.Point(133, 430);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(48, 20);
+            this.lbltexto.TabIndex = 10;
+            this.lbltexto.Text = "Texto";
+            this.lbltexto.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltexto);
+            this.Controls.Add(this.lblpromedio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnvalidar);
+            this.Controls.Add(this.txtn5);
+            this.Controls.Add(this.txtn4);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.TextBox txtn3;
+        private System.Windows.Forms.TextBox txtn4;
+        private System.Windows.Forms.TextBox txtn5;
+        private System.Windows.Forms.Button btnvalidar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblpromedio;
+        private System.Windows.Forms.Label lbltexto;
+    }
+}
+
---
+++ Notas/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnvalidar_Click(object sender, EventArgs e)
+        {
+            float n1 = float.Parse(txtn1.Text);
+            float n2 = float.Parse(txtn2.Text);
+            float n3 = float.Parse(txtn3.Text);
+            float n4 = float.Parse(txtn4.Text);
+            float n5 = float.Parse(txtn5.Text);
+            float promedio = n1 + n2 + n3 + n4 + n5;
+            float total = promedio / 5;
+
+            lblpromedio.Text = ("Su promedio es: " + total);
+            lblpromedio.Visible = true;
+
+            if (total <= 1)
+            {
+                lbltexto.Text= ("Reprueba la materia sin lograr realizar proceso de recuperación.");
+                lbltexto.Visible = true;
+            }
+
+            else
+            {
+            if (total <= 2.9)
+            {
+                lbltexto.Text = ("Reprueba la materia y puede hacer la habilitación");
+                lbltexto.Visible = true;
+            }
+            else
+            {
+            if (total <= 3.9)
+            {
+                lbltexto.Text = ("Aprueba la materia con plan de mejora");
+                lbltexto.Visible = true;
+            }
+            else
+            {
+            if (total <= 4.5)
+            {
+                  lbltexto.Text = ("Buen rendimiento");
+                  lbltexto.Visible = true;
+            }
+            else
+            {
+            if (total <= 4.9)
+            {
+                  lbltexto.Text = ("Excelente");
+                  lbltexto.Visible = true;
+            }
+            else
+            {
+            if (total == 5)
+            {
+                  lbltexto.Text = ("Graduado con honores");
+                  lbltexto.Visible = true;
+
+            }
+            }
+            }
+            }
+            }
+
+            }
+
+
+
+
+        }
+    }
+}
